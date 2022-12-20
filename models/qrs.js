b410a1427fb9b4befa160ead63b7add7423a3ab4
joelvappiani <joel.vappiani@gmail.com>
2022-12-20 22:54:26 +0100

    commit

--- models/qrs.js
+++ models/qrs.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const qrSchema = mongoose.Schema({
     userId : {type: mongoose.Schema.Types.ObjectId, ref:'users'},
-    infos : String,
+    infos : Array,
     isFav : Boolean,
     qrName : String,
     numScans : Number,
