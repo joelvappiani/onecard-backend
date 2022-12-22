26ab91495d0f6955a129e46ba71186af51bef66e
joelvappiani <joel.vappiani@gmail.com>
2022-12-22 17:02:33 +0100

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-require ('../models/connections');
+require ('./models/connections');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
